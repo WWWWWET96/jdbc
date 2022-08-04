5bdffd105a2f106bd7ecef9f2070bababd65847d
WWWWWET96 <wwwwwet96@naver.com>
2022-08-04 21:03:43 +0900

    findById,Update,Delete

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.net.http.HttpConnectTimeoutException;
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  *JDBC- DriverManager 사용
@@ -17,8 +18,6 @@ public class MemberRepositoryV0 {
 
         Connection con = null;
         PreparedStatement pstmt = null;
-
-
         try {
             con = getConnection();
             pstmt = con.prepareStatement(sql);
@@ -33,6 +32,76 @@ public class MemberRepositoryV0 {
            }finally {
             close(con,pstmt,null); //리소스 정리
         }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();//select문 실행
+            if(rs.next()){ //selectQuery결과 담고있는 rs에서 값 꺼내기
+                //next()통해 실제 데이터 있는 곳부터 호출되도록
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            }else{//select결과가 없으면
+                throw new NoSuchElementException("member not found memberId="+memberId);
+            }
+
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }finally { //사용한 순 역순으로 해제
+            close(con, pstmt, rs);
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);//sql에 대한 파라미터 바인딩(첫 번째 ?)
+            pstmt.setString(2,memberId);//파라미터 바인딩(두 번째 ?)
+            pstmt.executeUpdate();//실행
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }finally {
+            close(con,pstmt,null); //리소스 정리
+        }
+    }
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);//sql에 대한 파라미터 바인딩(첫 번째 ?)
+            pstmt.executeUpdate();//실행
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }finally {
+            close(con,pstmt,null); //리소스 정리
+        }
+
     }
     private void close(Connection con, Statement stmt, ResultSet rs){
         /** stmt.close()에서 error나면 con.close()실행 안되니까 try-catch로 감싸서 호출*/
